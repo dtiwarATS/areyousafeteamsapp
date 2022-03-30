5f9fbb6676bf88c72d7c7c09721aa06ff3983744
Sandesh Sawant <ssawant@ats360.com>
2022-03-31 00:11:48 +0530

    task-140

--- server/jobs/recurr-job.js
+++ server/jobs/recurr-job.js
@@ -8,8 +8,9 @@ const moment = require("moment-timezone");
 
 (async () => {
   //get filter job from database
-  let currentDateTime = moment(new Date()).utc().format('YYYY-MM-DD hh:mm');
-
+  //console.log("recurr job : start");
+  let currentDateTime = moment(new Date()).utc().format('YYYY-MM-DD HH:mm');
+  console.log("recurr job : currentDateTime - " + currentDateTime);
   let sqlJob = `SELECT A.ID AS INC_ID, B.ID AS SUB_EVENT_ID, B.CRON, B.TIMEZONE, A.INC_TYPE AS incType, A.INC_NAME, A.CREATED_BY AS createdById, ` +
                 `A.CREATED_BY_NAME AS createdByName, A.TEAM_ID, A.CHANNEL_ID, A.EVENT_END_DATE eventEndDate, A.EVENT_END_TIME eventEndTime  FROM MSTEAMSINCIDENTS A `+
                 `LEFT JOIN MSTEAMS_SUB_EVENT B ON A.ID = B.INC_ID `+
@@ -18,10 +19,12 @@ const moment = require("moment-timezone");
   let jobsToBeExecutedArr = await db.getDataFromDB(sqlJob);
   if(jobsToBeExecutedArr != null && jobsToBeExecutedArr.length > 0){
     // send msgs
+    //console.log("recurr job : no of record -" + jobsToBeExecutedArr.length);
     await Promise.all(
       jobsToBeExecutedArr.map(async (job) => {
         // send teams msg and change the runAt time of the job to next interval
         try {
+          
           const { CRON : cron, TIMEZONE: timeZone, INC_ID: incId, SUB_EVENT_ID: subEventId, TEAM_ID: teamId, INC_NAME: incTitle, eventEndDate, eventEndTime } = job;
           const options = { tz: timeZone };
 
@@ -33,6 +36,7 @@ const moment = require("moment-timezone");
             return true;
           }
 
+          //console.log("recurr job : " + incTitle);
           let eventMembersSql = `select [user_id] , [user_name]  from MSTeamsMemberResponses where inc_id = ${incId}`;
           let eventMembers = await db.getDataFromDB(eventMembersSql);
 
@@ -55,12 +59,14 @@ const moment = require("moment-timezone");
             }
           }          
         } catch (err) {
-          console.log(`ERROR occured while executing the job \nError: ${err}`);
+          //console.log(`ERROR occured while executing the job \nError: ${err}`);
         }
+        //console.log("recurr job : " + incTitle + " end");
       })
     );
   }  
 
+  //console.log("recurr job : end");
   // signal to parent that the job is done
   if (parentPort) parentPort.postMessage("done");
   else process.exit(0);
