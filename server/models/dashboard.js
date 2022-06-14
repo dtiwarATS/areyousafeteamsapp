5942a5046a80297303250e68f1257c96f5633a2b
Sandesh Sawant <ssawant@ats360.com>
2022-06-14 21:20:35 +0530

    card 177

--- server/models/dashboard.js
+++ server/models/dashboard.js
@@ -77,7 +77,88 @@ const getIncidentNameHeader = (eventName, addSeperator) => {
     }
  }
 
-const getUsersResponse = (members) => {
+ const getDetailUsersResponse = (membersUnsafe, membersNotResponded, membersSafe) => {
+    let membersUnsafeStr = membersUnsafe.join(", ");
+    let membersNotRespondedStr = membersNotResponded.join(", ");
+    let membersSafeStr = membersSafe.join(", ");
+    const detailsResponse = [
+        {
+            "type": "ColumnSet",
+            "columns": [
+                {
+                    "type": "Column",
+                    "width": 4,
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": `**🔴 Need Assistance: ${membersUnsafe.length}**`,
+                            "color": "attention"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": membersUnsafeStr,
+                            "isSubtle": true,
+                            "spacing": "none"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "ColumnSet",
+            "columns": [
+                {
+                    "type": "Column",
+                    "width": 4,
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": `**🟡 Not Responded: ${membersNotResponded.length}**`,
+                            "color": "default"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": membersNotRespondedStr,
+                            "isSubtle": true,
+                            "spacing": "none"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "ColumnSet",
+            "columns": [
+                {
+                    "type": "Column",
+                    "width": 4,
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": `**🟢 Safe: ${membersSafe.length}**`,
+                            "color": "good"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "wrap": true,
+                            "text": membersSafeStr,
+                            "isSubtle": true,
+                            "spacing": "none"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+    return detailsResponse;
+ }
+
+const getUsersResponse = (members, mentionUserEntities) => {    
     let result = {
         membersSafe: [],
         membersUnsafe: [],
@@ -85,7 +166,7 @@ const getUsersResponse = (members) => {
     };
 
     members.forEach((m) => {
-        const { userName, response, responseValue } = m;
+        const {userId, userName, response, responseValue } = m;
 
         if (response == "na" || response == false) {
             result.membersNotResponded.push(`<at>${userName}</at>`);
@@ -97,18 +178,35 @@ const getUsersResponse = (members) => {
                 result.membersUnsafe.push(`<at>${userName}</at>`);
             }
         }
+
+        const mention = {
+            type: "mention",
+            text: `<at>${userName}</at>`,
+            mentioned: {
+              id: userId,
+              name: userName,
+            },
+          };
+      
+        mentionUserEntities.push(mention);
     });
 
-    return {
+    const detailResponse = getDetailUsersResponse(result.membersUnsafe, result.membersNotResponded, result.membersSafe);
+    const shortResponse = {
         "type": "TextBlock",
         "wrap": true,
         "text": `🔴 Need Assistance: ${result.membersUnsafe.length} | 🟡 Not responded: ${result.membersNotResponded.length} | 🟢 Safe: ${result.membersSafe.length}`
+    }
+    return {
+        "detailResponse": detailResponse,
+        "shortResponse": shortResponse,
+        "mentionUserEntities": mentionUserEntities
     }
 }
 
 const getCreatedByObj = async (createdByNameId, allMembers) => {
     let createdByName = "";
-    if(createdByNameId != null){        
+    if(createdByNameId != null) {
         const usrObj = allMembers.find((m) => m.id = createdByNameId);    
         if(usrObj != null && usrObj.name != null){
             createdByName = usrObj.name;
@@ -117,24 +215,37 @@ const getCreatedByObj = async (createdByNameId, allMembers) => {
     
     return {
         "type": "TextBlock",
-        "text": `Created by **${createdByName}**`,
+        "text": `Created by **<at>${createdByName}</at>**`,
         "wrap": true
     }
 }
 
-const getDashboardActionBtnObj = (incId, companyData) => {
+const getDashboardActionBtnObj = (incId, companyData, detailResponse) => {
     return {
         "type": "ActionSet",
         "actions": [
             {
                 "type": "Action.ShowCard",
                 "card": {
-                    "type": "AdaptiveCard"
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.4",
+                    "type": "AdaptiveCard",
+                    "body": detailResponse,
+                    "msteams": {
+                        "entities": [{
+                            "type": "mention",
+                            "text": "<at>Sandesh Sawant</at>",
+                            "mentioned": {
+                              "id": "29:14xKzHoGhohgIpMI5zrDD2IuwD4XLWQHK-uN09QacAGO-r5MkSx2kuoKdB1hEKneuePknoF22_Oiwv0R0yz6KHA",
+                              "name": "Sandesh Sawant"
+                            }
+                        }]
+                    }                  
                 },
                 "title": "Show Details",
                 "data": {
                     "incId": `${incId}`,
-                    "companyData": companyData
+                    "companyData": companyData,
                 }               
             },
             {
@@ -227,12 +338,14 @@ const getIncidentTileDashboardCard = async (dashboardData, companyData, allTeamM
     
             const previousIndex = (eventIndex - 2);
             let body = [];
+            let uniquementionUserEntities = null;
             if(allIncData.length > eventIndex){       
                 let eventCount = 1;
                 let eventNum = 1;
                 if(eventIndex > 1){
                   eventNum = Number(eventIndex) + 1;
-                }                
+                }
+                const mentionUserEntities = [];
                 for(let i = (allIncData.length - eventIndex); i >= 1; i--){
                     if(eventCount > 2) {
                         break;
@@ -246,12 +359,12 @@ const getIncidentTileDashboardCard = async (dashboardData, companyData, allTeamM
                     const eventName = `${eventNum} ${incData.incTitle} ` + (incData.incType == "recurring" ? "(recurring)" : "");
                     const incNameHeader = getIncidentNameHeader(eventName, addSeperator);
                     const incStatusWithStartDate = getIncStatusWithStartDate("In-progress", incData.incCreatedDate);
-                    const userResponse = getUsersResponse(incData.members);
+                    const userResponseObj = getUsersResponse(incData.members, mentionUserEntities);
                     const createdBy = await getCreatedByObj(incData.incCreatedBy, allTeamMembers);
-                    const dashboardActionBtn = getDashboardActionBtnObj(incData.incId, companyData);
+                    const dashboardActionBtn = getDashboardActionBtnObj(incData.incId, companyData, userResponseObj.detailResponse, userResponseObj.mentionUserEntities);
                     body.push(incNameHeader);
                     body.push(incStatusWithStartDate);
-                    body.push(userResponse);
+                    body.push(userResponseObj.shortResponse);
                     body.push(createdBy);
                     body.push(dashboardActionBtn);
 
@@ -259,6 +372,11 @@ const getIncidentTileDashboardCard = async (dashboardData, companyData, allTeamM
                     eventIndex++;
                     eventNum++;
                 }
+
+                if(mentionUserEntities.length > 0){
+                    uniquementionUserEntities =  [...new Set(mentionUserEntities)];
+                }                
+
                 if(body.length > 0 && allIncData.length > 2){
                     const isPreviousBtnVisible = (eventIndex > 2); 
                     const isNextBtnVisible = (eventIndex < allIncData.length);
@@ -271,8 +389,23 @@ const getIncidentTileDashboardCard = async (dashboardData, companyData, allTeamM
                     type: "AdaptiveCard",
                     $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
                     version: "1.4",
-                    body: body             
+                    body: body,
+                    "msteams": {
+                        "entities": [{
+                            "type": "mention",
+                            "text": "<at>Sandesh Sawant</at>",
+                            "mentioned": {
+                              "id": "29:14xKzHoGhohgIpMI5zrDD2IuwD4XLWQHK-uN09QacAGO-r5MkSx2kuoKdB1hEKneuePknoF22_Oiwv0R0yz6KHA",
+                              "name": "Sandesh Sawant"
+                            }
+                        }]
+                    }         
                 };
+                // if(uniquementionUserEntities != null){
+                //     card["msteams"] = {
+                //         "entities": uniquementionUserEntities
+                //     }
+                // }
             }
         }
     }
