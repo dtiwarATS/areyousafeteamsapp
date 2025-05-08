12db59f23368bc0b9623f0fa734410650a6c2d34
Swapnil Chowkekar <schowkekar@ats360.com>
2025-05-08 17:31:38 +0530

    latest changes.

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -2269,8 +2269,8 @@ const sendAcknowledgeMsgToCreator = (connectorClient, incData, serviceUrl, conve
       baseUri: serviceUrl,
     });
   }
-  let msgText = `Thanks! Your <b>safety check message</b> has been sent to ${numberOfUsers} users./r/n
-Click on the <b>Dashboard tab</b> above to view the real-time safety status and access all features./r/n
+  let msgText = `Thanks! Your <b>safety check message</b> has been sent to ${numberOfUsers} users.<br />
+Click on the <b>Dashboard tab</b> above to view the real-time safety status and access all features.<br />
 For mobile, navigate to the <b>${teamName}</b> team -> <b>${channelName}</b> channel -> <b>Are You Safe?</b> tab`;
   let activity = MessageFactory.text(msgText);
   connectorClient.conversations
