705bb0cdd18dd0135e6acff157f017e7646b86bc
Devesh Tiwari <dtiwari@ats360.com>
2025-09-02 13:53:32 +0530

    Update log.js

--- server/utils/log.js
+++ server/utils/log.js
@@ -1,34 +1,32 @@
-const {
-    saveLog: saveLoginDB
-} = require("../db/dbOperations");
+const { saveLog } = require("../db/dbOperations");
 
 class AYSLog {
-    constructor() {
-        this.log = [];
-    }
+  constructor() {
+    this.log = [];
+  }
 
-    addLog = (message) => {
-        if (this.log != null) {
-            message = `<tr><td>${message}</td></tr>`;
-            this.log.push(message);
-        }
+  addLog = (message) => {
+    if (this.log != null) {
+      message = `<tr><td>${message}</td></tr>`;
+      this.log.push(message);
     }
+  };
 
-    saveLog = async (incId) => {
-        try {
-            if (incId == null || Number(incId) <= 0) {
-                incId = -1;
-            }
-            let logMessage = this.log.toString().replace(/'/g, "''");
-            logMessage = `<table>${logMessage}</table>`;
-            const logSql = `insert into MSTeamsLog ([inc_id], [log], [datetime]) values (${incId}, '${logMessage}', GETDATE())`;
-            await saveLoginDB(logSql);
-        } catch (err) {
-            console.log(err);
-        }
+  saveLog = async (incId) => {
+    try {
+      if (incId == null || Number(incId) <= 0) {
+        incId = -1;
+      }
+      let logMessage = this.log.toString().replace(/'/g, "''");
+      logMessage = `<table>${logMessage}</table>`;
+      const logSql = `insert into MSTeamsLog ([inc_id], [log], [datetime]) values (${incId}, '${logMessage}', GETDATE())`;
+      await saveLog(logSql);
+    } catch (err) {
+      console.log(err);
     }
+  };
 }
 
 module.exports = {
-    AYSLog
-}
+  AYSLog,
+};
